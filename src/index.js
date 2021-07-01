2f1db656d57d027d002079337ac8786f0ffa0097
saikrishnakingtyle <77505364+saikrishnakingtyle@users.noreply.github.com>
2021-07-02 00:26:12 +0530

    Changed the header Must be available in pwa

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import Heading from "./Header";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
+    <Heading />
     <App />
   </StrictMode>,
   rootElement
